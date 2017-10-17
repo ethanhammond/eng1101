ed71af8592de370db76ffc6d6da6be7da1300b68
Ethan Hammond <ethanhammond12099@gmail.com>
2017-10-17 11:44:17 -0400

    ICA 07a added

---
+++ ica_07A/ica_15_10.m
@@ -0,0 +1,20 @@
+%ICA 15-10
+%Ethan Hammond
+%Eng 1101 07A
+%10-17-17
+
+%Determine values of vectors used to calculate the properties of cones.
+
+diam=[8;9;15;8;6];
+ht=[14;7;9;8;11];
+sg=[2.7;1.2;2.2;7.85;19.3];
+rad=(diam(1:5)./2);
+vol=(pi*(rad(1:5).^2)).*(ht(1:5)./3);
+mass=(sg(1:5)./1000).*vol(1:5);
+avgCone=mean(vol(1:5));
+heaviestCone=max(mass(1:5));
+lightestCone=min(mass(1:5));
+[heaviestCone,I]=max(mass(:));
+heavyHt=ht(I);
+
+fprintf('The heaviest cone was %f kg and has a height of %f meters',heaviestCone, heavyHt);
