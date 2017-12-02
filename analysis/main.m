70c3328d26b1b7a6a2c516b4766babc99c1d923a
Ethan <ethanhammond12099@gmail.com>
2017-12-02 14:39:03 -0500

    Started numerical integration

--- analysis/main.m
+++ analysis/main.m
@@ -2,6 +2,8 @@
 % ENG 1101 L15 T6
 % Wind Turbine Analysis
 
+%Make resistance, R_model, h_model, R_
+
 clear,clc
 close all
 
@@ -19,32 +21,38 @@ resistance=987;
 %resistance=input('Please enter the resistance value for your model in ohms:\n');
 
 %Prototype Analysis
-
 %Declare constants for pTheoretical
 rho=1.2;                        %kg/m^3, air density
 Nb=.95;                         %unitless, bearing efficiency
 Cp=.35;                         %unitless, power coefficient
 Ng=.8;                          %unitless, generator efficiency
-R_model=0.06985;                %meters, VAWT model radius
-h=0.1778;                       %meters, VAWT model height
 
-%Get blade length from user
-%R_model=input('Please enter the radial length of the model blade in meters:\n');
+% Model Dimensions
+% Model built to 7/24th height and 6/11 diameter scale of typical Savonius
+% style VAWT
+R_model=0.06985;                %meters, VAWT model outer radius
+H_model=0.1778;                 %meters, VAWT model blade height
+%R_model=input('Please enter the radius of the blade (from center of VAWT to edge in meters) (group value was .06985)');
+%H_model=input('Please enter the height of your VAWT blades in meters (group value was .1778)');
+A_model=2*R_model*H_model;      %meters squared, VAWT swept area
 
-%Get blade height from user
-%h=input('Please enter the height of the VAWT blade in meters:\n');
+%Full Dimensions
+%Scale factor taken from diagram of savonius VAWT located here: http://www.fieldlines.com/index.php?topic=139867.0
+R_full=1.76*R_model;
+H_full=3.42857*H_model;
+A_full=R_full*H_full;
 
 %Calculate pTheoretical for VAWT
-%Equation taken from Brusca, S., Lanzafame, R. & Messina, M. Int J Energy Environ Eng (2014) 5: 333. https://doi.org/10.1007/s40095-014-0129-x
-pTheoretical=.5*rho*velocity.^3*2*R_model*h*Cp;
+%Equation modified from Brusca, S., Lanzafame, R. & Messina, M. Int J Energy Environ Eng (2014) 5: 333. https://doi.org/10.1007/s40095-014-0129-x
+pTheoretical=.5*rho*velocity.^3*2*R_model*H_model*Cp*Ng*Nb;
 
 %Calculate pObserved
 pObserved=(voltage.^2)/resistance;
 
 %Find polyfit of pObserved
-polyfitPobs=polyfit(log(velocity),log(pObserved),1);
-mPobs=polyfitPobs(1);
-bPobs=polyfitPobs(2);
+pFit=polyfit(log(velocity),log(pObserved),1);
+mPobs=pFit(1);
+bPobs=pFit(2);
 pObsFit=velocity.^mPobs*exp(bPobs);
 
 %Find polyfit of pTheoretical
@@ -69,7 +77,7 @@ printTheoretical = sprintf('P = %fx^{%f}',bTheor,mTheor);
 text(1.1,.015,printTheoretical)
 hold off;
 
-%Scale Up Turbine Analysis
+%Scaled Up Turbine Analysis
 
 %Load Weather Data
 weatherRaw = load('weather.txt');
@@ -78,13 +86,21 @@ windSpeedMPH = weatherRaw(:,4); %mph
 windSpeedMS = weatherRaw(:,4)*0.44704; %m/s
 
 %Full Scale Model Calculations
-%R_full=input('Please enter the radial length of the full scale blade in meters:\n');
 
-%isRunning Table
+% Adjust constant bPobs for scaled model
+bFull=(bPobs*A_full)/A_model;
+
+% Calculate time step
+for i=2:1:length(time)
+    %Note: This time step remains the same for all iterations
+    dt=time(i)-time(i-1);
+end
+
 isRunning = 1;
 fprintf('BEGIN ERROR LOG\n');
 fprintf('Time(min)\tPower Generation Status (On/Off)\tReason\n');
 for i = 1:1:length(weatherRaw)
+  %isRunning Table
   changeCheck = isRunning;
   if windSpeedMPH(i) > 45
       isRunning = 0;
@@ -98,16 +114,13 @@ for i = 1:1:length(weatherRaw)
       isRunning = 1;
       status = 'ON';
       reason = 'Speed is within range';
+      
+      % Calculate pScaled based on empirical function of pFit
+
   end
   if changeCheck ~= isRunning
       fprintf('%.0f %25s %35s\n',i,status,reason);
   end
+  
 end
 fprintf('END ERROR LOG\n');
-
-% Calculate time step
-for i=2:1:length(time)
-    %Note: This time step remains the same for all iterations
-    dt=time(i)-time(i-1);
-end
-
